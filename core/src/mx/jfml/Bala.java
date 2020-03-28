40723d16858d5dba95a7cabeb45b18e116630a32
JulioLugo9 <a01748403@itesm.mx>
2020-03-28 16:24:30 -0600

    Se integro la clase Bala (Lo mismo que el anterior/borrar cosas no utiles)

--- core/src/mx/jfml/Bala.java
+++ core/src/mx/jfml/Bala.java
@@ -1,11 +1,5 @@
 package mx.jfml;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Bala extends Objeto {
 
@@ -15,7 +9,7 @@ public class Bala extends Objeto {
 	public Bala(Texture textura, float x, float y) {
 		super(textura, x, y);
 	}
-
+	//Mover
 	public void mover(float dt){
 		float dx = vx*dt;
 		sprite.setX(sprite.getX()+dx);
