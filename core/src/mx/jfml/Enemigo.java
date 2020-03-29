df42c908dfda34a505da0cd924c7ad0593f2be52
Jonathan Rosas <A01377942@itesm.mx>
2020-03-29 13:58:18 -0600

    Enemigo para el juego

---
+++ core/src/mx/jfml/Enemigo.java
@@ -0,0 +1,22 @@
+package mx.jfml;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Enemigo extends Objeto {
+    private float life = 200f;
+    private float vx = 360;
+
+    public Enemigo(Texture textura, float x, float y) {
+        super(textura, x, y);
+    }
+    public void mover(float dt){
+        float dx = vx*dt;
+        sprite.setX(sprite.getX()+dx);
+    }
+    public float getLife(){
+        return life;
+    }
+    public void setLife(float dano){
+        this.life = life-dano;
+    }
+}
