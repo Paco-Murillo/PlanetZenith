f9daf6f12f3cb5a59806ffe4a0da229a0f96d0bc
JulioLugo9 <a01748403@itesm.mx>
2020-05-05 19:04:28 -0500

    vERSION NO BUGEADA

--- core/src/mx/jfml/Jefe.java
+++ core/src/mx/jfml/Jefe.java
@@ -7,7 +7,7 @@ public class Jefe extends Enemigo {
     private int vidaOriginal;
 
     public Jefe(Texture textura, float x, float y, float vx, float vy, float vida, World mundo) {
-        super(textura, x, y, vx, vy, vida, mundo);
+        super(textura, x, y, vx, vy, vida, mundo,200);
         vidaOriginal = (int)vida;
     }
 
