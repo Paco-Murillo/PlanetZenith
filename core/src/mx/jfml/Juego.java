b7361894738b2e7673dcec547ef94247d03f3933
EJ-G0mez <A01377235@itesm.mx>
2020-05-21 14:43:01 -0500

    Reduci el volumen de la musica

--- core/src/mx/jfml/Juego.java
+++ core/src/mx/jfml/Juego.java
@@ -21,7 +21,7 @@ public class Juego extends Game {
 	 * @value seleccionaNivel Permite saber que nivel cargar
 	 */
 	private final AssetManager assetManager = new AssetManager();
-	private AudioManejador audioManejador = new AudioManejador(false, 1f,1f);
+	private AudioManejador audioManejador = new AudioManejador(false, .75f,.75f);
 	private SeleccionaNivel seleccionaNivel;
 
 	@Override
